04a973f961aef7c21c75b61f74275082f767a825
Zuxel <anicg124@yandex.ru>
2022-10-13 18:47:02 +0300

    выполнил задание 3.7 работает с датой

--- data.php
+++ data.php
@@ -8,36 +8,42 @@ $goods = [
         "category" => "Доски и лыжи",
         "price" => 10999,
         "url_addres" => "img/lot-1.jpg",
+        "exp_date" => "2022-10-20",
     ],
     [
         "name" => "DC Ply Mens 2016/2017 Snowboard", 
         "category" => "Доски и лыжи", 
         "price" => 159999,
         "url_addres" => "img/lot-2.jpg",
+        "exp_date" => "2022-10-21",
     ],
     [
         "name" => "Крепления Union Contact Pro 2015 года размер L/XL", 
         "category" => "Крепления", 
         "price" => 8000,
         "url_addres" => "img/lot-3.jpg",
+        "exp_date" => "2022-10-22",
     ],
     [
         "name" => "Ботинки для сноуборда DC Mutiny Charocal", 
         "category" => "Ботинки", 
         "price" => 10999,
         "url_addres" => "img/lot-4.jpg",
+        "exp_date" => "2022-10-23",
     ],
     [
         "name" => "Куртка для сноуборда DC Mutiny Charocal", 
         "category" => "Одежда", 
         "price" => 7500,
         "url_addres" => "img/lot-5.jpg",
+        "exp_date" => "2022-10-30",
     ],
     [
         "name" => "Маска Oakley Canopy", 
         "category" => "Разное", 
         "price" => 5400,
         "url_addres" => "img/lot-6.jpg",
+        "exp_date" => "2022-10-13 16:20",
     ],
 ];
 
--- functions.php
+++ functions.php
@@ -4,4 +4,12 @@ function price_format($price) {
         $price = ceil($price);
         return number_format($price, 0, ",", " ") . " ₽";
 
-}
+};
+
+function get_dt_range ($date) {
+                        $date_now = strtotime("now");
+                        $date_end = strtotime($date);
+                        $diff_m= floor((($date_end - $date_now) % 3600)/60);
+                        $diff_h= ($date_end - $date_now -($date_end - $date_now) % 3600) / 3600;
+                        return [str_pad($diff_h, 2, "0", STR_PAD_LEFT), str_pad($diff_m, 2, "0", STR_PAD_LEFT)];
+};

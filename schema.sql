e8de67ba9ca2c1879439467ff452fbaa2413058c
Zuxel <anicg124@yandex.ru>
2022-11-08 18:43:20 +0300

    исправляю файл базы данных по заданию 4.10

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS yeticave;
 
 CREATE DATABASE yeticave
-	DEFAULT CHARACTER SET utf-8
+	DEFAULT CHARACTER SET utf8
 	DEFAULT COLLATE utf8_general_ci;
 
 USE yeticave;
@@ -10,7 +10,7 @@ USE yeticave;
 CREATE TABLE categories ( 
 	category_id INT PRIMARY KEY AUTO_INCREMENT,
 	character_code VARCHAR(128) UNIQUE,
-	name_category VARCHAT(50)
+	name_category VARCHAR(50)
 );
 
 
@@ -19,12 +19,12 @@ CREATE TABLE users (
 	date_of_registration DATE,
 	email VARCHAR(30),
 	user_name VARCHAR(30),
-	password CHAR(256),
+	password CHAR(255),
 	contacts TEXT,
-	lots_id,
-	bets_id
-	FOREING KEY (lots_id) REFERENCES lots(lots_id),
-	FOREING KEY(bets_id) REFERENCES bets(bets_id)
+	lots_id INT,
+	bets_id INT,
+	FOREIGN KEY (lots_id) REFERENCES lots(lots_id),
+	FOREIGN KEY(bets_id) REFERENCES bets(bets_id)
 );
 
 CREATE TABLE lots(
@@ -36,20 +36,20 @@ CREATE TABLE lots(
 	start_price DECIMAL(8, 2),
 	date_finish DATE,
 	step INT,
-	user_id,
-	winner_id,
-	category_id,
-	FOREING KEY (user_id) REFERENCES users(user_id),
-	FOREING KEY (winner_id) REFERENCES users(user_id),
-	FOREING KEY (caregory_id) REFERENCES categories(caregory_id)
+	user_id INT,
+	winner_id INT,
+	category_id INT,
+	FOREIGN KEY (user_id) REFERENCES users(user_id),
+	FOREIGN KEY (winner_id) REFERENCES users(user_id),
+	FOREIGN KEY (caregory_id) REFERENCES categories(caregory_id)
 );
 
 CREATE TABLE bets (
 	bets_id INT PRIMARY KEY AUTO_INCREMENT,
 	date_bet DATETIME,
 	price_bet DECIMAL(8, 2),
-	user_id,
-	lots_id,
-	FOREING KEY(user_id) REFERENCES users(user_id),
-	FOREING KEY(lots_id) REFERENCES lots(lots_id)
+	user_id INT,
+	lots_id INT,
+	FOREIGN KEY(user_id) REFERENCES users(user_id),
+	FOREIGN KEY(lots_id) REFERENCES lots(lots_id)
 );

9e9b0bb1ff07b21028f24c95d2fcb985a3b785b4
Zuxel <anicg124@yandex.ru>
2023-01-31 18:59:26 +0300

    продолжил делать упражнение 5.6 сделал в главном файле подсоединение к базе и запрос категорий

--- index.php
+++ index.php
@@ -7,14 +7,21 @@ require_once('helpers.php');
 
 
 
-/*$link = mysqli_connect("localhost", "root", "root", "yeticave");
+$link = mysqli_connect("localhost", "root", "root", "yeticave");
+mysqli_set_charset($link, "utf8");
 if ($link == false) {
    print("Ошибка подключения: " . mysqli_connect_error());
 }
 else {
-   print("Соединение установлено");
+   $sql_categories = "SELECT character_code, name_categories FROM categories";
+   $result = mysqli_query($link, $sql_categories);
+   if($result) {
+      $categories = mysql_fetch_all($result, MYSQLI_ASSOC);
+   } else {
+      $error = mysqli_error($link);
+   }
     
-}*/
+}
 
 
 $page_content = include_template("main.php", [
